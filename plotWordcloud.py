25c068d548ead25f5ab20118eae1faefed12f251
godplusone <shenjiayi@buaa.edu.cn>
2020-10-13 14:20:48 +0800

    no message

--- plotWordcloud.py
+++ plotWordcloud.py
@@ -16,7 +16,8 @@ def generate_wordcloud(text):
     # alice_mask = np.array(Image.open(path.join(d, "Images//alice_mask.png")))
     font_path=path.join(d,"font//msyh.ttf")
     # stopwords = set(STOPWORDS)
-    stopwords = {"Learning","via", "Network", "Neural Network", "for", "and", "with", "in", "of", "the", "to", "by", "An", "A", "on", "Method", "Non", "from"}
+    stopwords = {"Learning","via", "Network", "Neural Network", "for", "and", "with", "in", "of", "the", "to", "by", "An", "A", "on", "Method", "Non", "from",
+                 "Neural", "Networks", "a", "The", "On", "learning"}
     
     wc = WordCloud(width=800, height=400, background_color="white",# 设置背景颜色
            max_words=1000, # 词云显示的最大词数  
@@ -29,6 +30,9 @@ def generate_wordcloud(text):
     dit = {}
     text_split = text.split(" ")
     for word in text_split:
+        if word.islower():
+          word = word.capitalize()
+        pdb.set_trace()
         if word in dit and word not in stopwords:
             dit[word] += 1
         else:
