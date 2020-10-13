5523c03bea553cf81278c89629e4c4c9859035ce
godplusone <shenjiayi@buaa.edu.cn>
2020-10-13 13:08:08 +0800

    no message

--- demo.py
+++ demo.py
@@ -3,16 +3,31 @@
 from os import path
 import chnSegment
 import plotWordcloud
+import pdb
 
 
 if __name__=='__main__':
 
-    # 读取文件
+    # # 读取文件
+    # d = path.dirname(__file__)
+    # #  text = open(path.join(d, 'doc//十九大报告全文.txt')).read()
+    # # text = open(path.join(d,'doc//alice.txt'), 'r', encoding='utf-8').read()
+
+    # text = open(path.join(d,'doc//nips2020paperlist.txt'), 'r', encoding='utf-8').read()
+    
+
+    # # 若是中文文本，则先进行分词操作
+    # # text=chnSegment.word_segment(text)
+    
+    # # 生成词云
+    # plotWordcloud.generate_wordcloud(text)
+
+
     d = path.dirname(__file__)
-    #  text = open(path.join(d, 'doc//十九大报告全文.txt')).read()
-    # text = open(path.join(d,'doc//alice.txt'), 'r', encoding='utf-8').read()
-    text = open(path.join(d,'doc//nips2020paperlist.txt'), 'r', encoding='utf-8').read()
-    #  text="付求爱很帅并来到付求爱了网易研行大厦很帅 很帅 很帅"
+    f = open(path.join(d,'doc/nips2020paperlist.txt'), 'r', encoding='utf-8')
+
+    pdb.set_trace()
+    
 
     # 若是中文文本，则先进行分词操作
     # text=chnSegment.word_segment(text)
