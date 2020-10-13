29838a6d73367d28f1c7ef1e71c908bd2f215cc4
godplusone <shenjiayi@buaa.edu.cn>
2020-10-13 13:01:03 +0800

    no message

--- plotWordcloud.py
+++ plotWordcloud.py
@@ -12,12 +12,12 @@ def generate_wordcloud(text):
     '''
     # 设置显示方式
     d=path.dirname(__file__)
-    alice_mask = np.array(Image.open(path.join(d, "Images//alice_mask.png")))
+    # alice_mask = np.array(Image.open(path.join(d, "Images//alice_mask.png")))
     font_path=path.join(d,"font//msyh.ttf")
     stopwords = set(STOPWORDS)
     wc = WordCloud(background_color="white",# 设置背景颜色
            max_words=2000, # 词云显示的最大词数  
-           mask=alice_mask,# 设置背景图片       
+           # mask=alice_mask,# 设置背景图片       
            stopwords=stopwords, # 设置停用词
            font_path=font_path, # 兼容中文字体，不然中文会显示乱码
                   )
