cbdb65a88e6060c3000d0db87f4e6d7eb697cb40
godplusone <shenjiayi@buaa.edu.cn>
2020-10-13 20:15:34 +0800

    no message

--- plotWordcloud.py
+++ plotWordcloud.py
@@ -22,8 +22,7 @@ def generate_wordcloud(text):
     dit = {}
     text_split = text.split(" ")
     for word in text_split:
-        word.lower():
-        
+        word.lower()
         if word in dit and word not in stopwords:
             dit[word] += 1
         else:
